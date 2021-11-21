c6512d2190247734d17d1a70df213d7ba8f77250
crazy-medic <vineethtv001@outlook.com>
2021-11-21 08:40:40 +0530

    Created a table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,10 @@
 ---UC1CreateDatabase----
 create database payroll_service72;
 use payroll_service72;
+---UC2 create table---
+Create table emp_payroll(
+	id int IDENTITY(1,1) PRIMARY KEY,
+	name varchar(200),
+	salary float,
+	JoiningDate date
+);
